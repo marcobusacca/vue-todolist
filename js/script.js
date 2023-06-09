df07abcace29ae223c9cb4cf0c6266215c30d541
marcobusacca <marcobusacca01@gmail.com>
2023-06-09 15:21:30 +0200

    [FEAT] Boilerplate JavaScript vue.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// RICHIAMO FUNZIONE CREATE_APP DA VUE.JS
+const {createApp} = Vue;
+
+// FUNZIONE CREATE_APP
+createApp({
+    // FUNZIONE DATA CON OGGETTO RETURN
+    data(){
+        return{
+
+        }
+    },
+    // FUNZIONE METHODS
+    methods:{
+        
+    }
+}).mount('#app'); // COLLEGO L'APP VUE.JS AL DOM HTML
